0b01fe2e415353243f34433bdd0fa9111e40d27a
Omi Co., Ltd <95911788+Omi-Co-Ltd@users.noreply.github.com>
2022-12-17 16:07:24 +0900

    Update omiMe.js

--- omiMe.js
+++ omiMe.js
@@ -1,7 +1,7 @@
 function goLoginPage(){
 	passwd=document.formno1.passwd.value;
 	if(passwd=="OmiPS0303"){
-		location.href="/p069-login1234/";
+		location.href="https://omi-co-ltd.github.io/on/";
 	}else{
 		alert('パスワードが違います')；
 	}
