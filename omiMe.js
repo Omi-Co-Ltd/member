06e2afa617f26f90bec8dd4357685d145b489fa5
Omi Co., Ltd <95911788+Omi-Co-Ltd@users.noreply.github.com>
2022-12-17 16:57:49 +0900

    Delete omiMe.js

--- omiMe.js
+++
@@ -1,8 +0,0 @@
-function goLoginPage(){
-	passwd=document.formno1.passwd.value;
-	if(passwd=="OmiPS0303"){
-		location.href="https://omi-co-ltd.github.io/on/";
-	}else{
-		alert('パスワードが違います')；
-	}
-}
